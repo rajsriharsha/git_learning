03142c9f1197a560e0210dd69374419501541a95
Harsha <rajsriharsha@gmail.com>
2022-08-01 20:12:10 +1000

    Feature 1 and Feature 2

--- scripts.py
+++ scripts.py
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
+
 # First Developer Commit A1
 # First Developer Commit A2
-=======
+
 # Second Developer Commit B1
 # Second Develoepr Commit B2
 # Second Developer Commit B3
 
 # Second Developer Commit B4
 # Second Developer Commit B5
->>>>>>> main
+
+# 31-July-2022 - Feature 1
+# 31-July-2022 - Feature 2
